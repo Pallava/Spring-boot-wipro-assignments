959c47bad46717cee4f4e7275707d5549b196090
pallava <pallava@pallava-PC>
2020-05-27 21:40:06 +0300

    search feature added

--- foodcart/src/main/java/com/foodcart/repository/FoodDetailsRepository.java
+++ foodcart/src/main/java/com/foodcart/repository/FoodDetailsRepository.java
@@ -1,10 +1,18 @@
 package com.foodcart.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.foodcart.model.FoodDetails;
+@CrossOrigin(origins = "*")//any request coming from other sources u need to allow 
 @RepositoryRestResource(collectionResourceRel = "foodDetailses" ,path="food-details") //overriding rest end points
 public interface FoodDetailsRepository extends JpaRepository<FoodDetails, Integer>{
 
+	@RestResource(path = "foodCategoryId")
+	Page <FoodDetails> findByFoodTypeId(@Param("id") Integer categoryId,Pageable pageable);
 }
